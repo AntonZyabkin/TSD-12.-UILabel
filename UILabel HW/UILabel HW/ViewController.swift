42baf4c10d3e9f835185e7c7e6edce05e741a27a
AntonZyabkin <zyabkinantonv@gmail.com>
2022-06-15 12:47:45 +0300

    second commit

--- UILabel HW/UILabel HW/ViewController.swift
+++ UILabel HW/UILabel HW/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alignmentSegmentedControll: UISegmentedControl!
+    @IBOutlet weak var mainLabelOutlet: UILabel!
+    @IBOutlet weak var FontTypePicker: UIPickerView!
+    @IBOutlet weak var numberOflinesPicjer: UIPickerView!
+
+    @IBOutlet weak var fontColorPicker: UIPickerView!
+    @IBOutlet weak var fontSizeSLider: UISlider!
+    
+    @IBOutlet weak var shadowSwitch: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func addTextButtonAction(_ sender: UIBarButtonItem) {
+        
+        let addTextAlert = UIAlertController (title: "Let's play with text", message: "Add some text in preview label", preferredStyle: .alert)
+        addTextAlert.addTextField() { (text) in
+            text.placeholder = "Input text"
+        }
+        let okAlertAction = UIAlertAction(title: "Add text", style: .default) { [self] _ in
+            if let text = addTextAlert.textFields?.first?.text {
+                self.mainLabelOutlet.text = text
+            }
+        }
+        let canselAlertAction = UIAlertAction (title: "Cancel", style: .cancel)
+        addTextAlert.addAction(okAlertAction)
+        addTextAlert.addAction(canselAlertAction)
+        
+        self.present(addTextAlert, animated: true)
+    }
+    
+    @IBAction func alignmentSegmentedControllAction(_ sender: Any) {
+    }
+    
+    @IBAction func fontSizeSliderAction(_ sender: UISlider) {
+    }
+    @IBAction func shadowSwitchAction(_ sender: UISwitch) {
+    }
 }
 
